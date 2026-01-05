9c332cfa8b8b624e33ac138984d4944bf0d194ff
Hossain-Shifat <161940464+hossain-shifat@users.noreply.github.com>
2026-01-05 18:10:02 +0600

    Add delete api in admin page

--- index.js
+++ index.js
@@ -356,13 +356,7 @@ async function run() {
         res.send(result)
     })
 
-    //delete task by admin
-    app.delete('/tasks/admin/:id', verifyFirebaseToken, veryfyAdmin, async (req, res) => {
-        const id = req.params.id
-        const query = { _id: new ObjectId(id) }
-        const result = await taskCollection.deleteOne(query)
-        res.send(result)
-    })
+    
 
     //get buyer stats
     app.get('/tasks/stats/buyer', verifyFirebaseToken, veryfyBuyer, async (req, res) => {
