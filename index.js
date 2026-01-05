14d7ece79be5478003740838e0c47348bf3da338
Hossain-Shifat <161940464+hossain-shifat@users.noreply.github.com>
2026-01-05 19:28:39 +0600

    Add bufferload

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const port = process.env.PORT || 3000
 
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./tasknest-firebase-adminsdk.json");
+// const serviceAccount = require("./tasknest-firebase-adminsdk.json");
+
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
